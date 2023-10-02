54d0b0ca66fd107251336616556f6530dcf22030
marcos <marcosvinicios4132@gmail.com>
2023-10-02 09:53:14 -0300

    Configuring eureka clients

--- hr-payroll/src/main/java/com/dev/marcos/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/dev/marcos/hrpayroll/HrPayrollApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Habilitando clientFeign na nossa aplicação
+ */
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
