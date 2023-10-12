7cd554cf08f3c34ce1b74bbc019cca9423a41379
marcos <marcosvinicios4132@gmail.com>
2023-10-11 21:09:11 -0300

    Configuring hr-worker config properties

--- hr-config-server/src/main/java/com/dev/marcos/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/dev/marcos/hrconfigserver/HrConfigServerApplication.java
@@ -2,7 +2,21 @@ package com.dev.marcos.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Este será um serviço onde será centralizado 
+ * as configurações dos microsserviços da aplicação
+ * 
+ * Estas configurações (no caso deste projeto em específico)
+ * encontram-se no github
+ *
+ * Antes de um microsserviço subir no eureka-server, se ele estiver configurado
+ * como um cliente de um servidor de confIGURAÇÃO, ele irá buscar
+ * as configurações antes de subir 
+ */
+
+@EnableConfigServer //Habilitando este projeto como um servidor de configuração
 @SpringBootApplication
 public class HrConfigServerApplication {
 
