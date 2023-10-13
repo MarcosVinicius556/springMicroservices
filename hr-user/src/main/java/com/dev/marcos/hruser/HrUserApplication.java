b3a903c7d7415c8f6074b3f3c46f2248035075a5
marcos <marcosvinicios4132@gmail.com>
2023-10-13 14:30:35 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/com/dev/marcos/hruser/HrUserApplication.java
@@ -0,0 +1,18 @@
+package com.dev.marcos.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Projeto responsável por manter o cadastro dos usuários e fornecer 
+ * dados e funções relacionadas aos usuários
+ */
+
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
