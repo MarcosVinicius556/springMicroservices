7ca72f67b89fed2ecf01a46ed51b45d871c99be1
marcos <marcosvinicios4132@gmail.com>
2023-10-13 10:23:37 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/dev/marcos/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/dev/marcos/hrworker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,17 @@ import com.dev.marcos.hrworker.services.WorkerService;
 
 @RestController
 @RequestMapping( "/workers" )
+/**
+ * A anotação refreshScope deve ser adicionada a 
+ * toda classe que tiver acesso a qualquer tipo
+ * de configuração do servidor.
+ * Isto irá permitir que quando uma alteração
+ * nas configurações seja realizada, o projeto
+ * carregue as novas sem a necessidade de derrubar
+ * e subir os projetos novamente
+ * 
+ */
+@RefreshScope
 public class WorkerResource {
 	
 	
